2ecf9b40442d2154004792ab1765f294cbb8608f
Derek Morr <derek.morr.test@gmail.com>
2017-03-12 13:02:37 -0400

    don't reimplement columnTo logic; use an underlying Column[T] to do it

--- src/main/scala/refined/anorm/package.scala
+++ src/main/scala/refined/anorm/package.scala
@@ -3,9 +3,8 @@ package refined
 import java.sql.PreparedStatement
 
 import scala.language.higherKinds
-import scala.reflect.ClassTag
 
-import _root_.anorm.{Column, MetaDataItem, ParameterMetaData, ToStatement, TypeDoesNotMatch}
+import _root_.anorm.{Column, ParameterMetaData, ToStatement, TypeDoesNotMatch}
 import eu.timepit.refined.api.{RefType, Validate}
 
 /**
@@ -17,26 +16,25 @@ package object anorm {
     * Implicit support for deserializing a column from a database record into a refined type.
     * @param refType The refinement carrier type
     * @param validate A [[Validate]] instance for the base type and the type-level refinement predicate.
+    * @param baseColumnTo The Anorm [[Column]] for the refinement's base type (e.g. Int, String, etc)
     * @tparam F The refined type
     * @tparam T The base type that's being refined.
     * @tparam P The type-level refinement predicate
     * @return A Column for type F[T, P] (e.g., Refined[Int, Positive])
     */
-  implicit def columnToRefType[F[_, _], T: ClassTag, P](implicit
-                                                        refType: RefType[F],
-                                                        validate: Validate[T, P]): Column[F[T, P]] = {
+  implicit def columnToRefType[F[_, _], T, P](implicit
+                                              refType: RefType[F],
+                                              validate: Validate[T, P],
+                                              baseColumnTo: Column[T]): Column[F[T, P]] = {
+
     Column.nonNull { (value, meta) =>
-      val MetaDataItem(qualified, _, clazz) = meta
-      val className = value.asInstanceOf[AnyRef].getClass.toString
-      value match {
-        case t: T =>
-          refType.refine[P](t) match {
-            case Left(errMsg) =>
-              Left(TypeDoesNotMatch(s"Value $t of type $className does not satisfy refinement predicate: $errMsg"))
-            case Right(r) => Right(r)
-          }
-        case _ =>
-          Left(TypeDoesNotMatch(s"Cannot convert $value of type $className to $clazz for column $qualified"))
+      baseColumnTo(value, meta).toEither.flatMap { value =>
+        val className = value.asInstanceOf[AnyRef].getClass.toString
+        refType.refine[P](value) match {
+          case Left(errMsg) => Left(TypeDoesNotMatch(
+            s"Value $value of type $className for column ${meta.column.qualified} does not satisfy refinement predicate: $errMsg"))
+          case Right(r) => Right(r)
+        }
       }
     }
   }
